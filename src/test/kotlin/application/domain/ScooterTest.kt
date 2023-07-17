b2e9e8faf8d62564a3d6e344088f6b144d093b35
sergiocostamoreno <47816978+sergiocostamoreno@users.noreply.github.com>
2023-07-17 21:04:40 +0200

    Scooter tests

--- src/test/kotlin/application/domain/ScooterTest.kt
+++ src/test/kotlin/application/domain/ScooterTest.kt
@@ -2,28 +2,74 @@ package application.domain
 
 import com.example.application.domain.Scooter
 import com.example.application.domain.ScooterId
+import com.example.application.domain.ScooterInvalidStatus
 import com.example.application.domain.ScooterStatus.LOCKED
 import com.example.application.domain.ScooterStatus.RUNNING
 import com.example.application.domain.UserId
+import com.example.fixtures.isLeftWith
 import com.example.fixtures.isRightWith
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class ScooterTest {
 
-    private val scooter = Scooter(
-        ScooterId(1),
-        LOCKED,
-        UserId("A")
-    )
+    @Nested
+    inner class RunScooter {
 
-    @Test
-    fun `should run Scooter`() {
-        val userId = UserId("B")
-        val expected = scooter.copy(status = RUNNING, lastRider = userId)
+        private val scooter = Scooter(ScooterId(1), LOCKED, UserId("A"))
+        private val lastRider = UserId("B")
 
-        val result = scooter.running(userId)
+        @Test
+        fun `should run Scooter`() {
+            val expected = scooter.copy(status = RUNNING, lastRider = lastRider)
 
-        assertThat(result).isRightWith(expected)
+            val result = scooter.running(lastRider)
+
+            assertThat(result).isRightWith(expected)
+        }
+
+        @Test
+        fun `should fail when Scooter is not locked`() {
+            val scooter = scooter.copy(status = RUNNING)
+
+            val result = scooter.running(lastRider)
+
+            assertThat(result).isLeftWith(ScooterInvalidStatus)
+        }
+    }
+
+    @Nested
+    inner class LockScooter {
+
+        private val userId = UserId("A")
+        private val scooter = Scooter(ScooterId(1), RUNNING, userId)
+
+        @Test
+        fun `should lock Scooter`() {
+            val expected = scooter.copy(status = LOCKED)
+
+            val result = scooter.locked(userId)
+
+            assertThat(result).isRightWith(expected)
+        }
+
+        @Test
+        fun `should fail when Scooter is not running`() {
+            val scooter = scooter.copy(status = LOCKED)
+
+            val result = scooter.locked(userId)
+
+            assertThat(result).isLeftWith(ScooterInvalidStatus)
+        }
+
+        @Test
+        fun `should fail when UserId was not the Scooter rider`() {
+            val userId = UserId("B")
+
+            val result = scooter.locked(userId)
+
+            assertThat(result).isLeftWith(ScooterInvalidStatus)
+        }
     }
 }
