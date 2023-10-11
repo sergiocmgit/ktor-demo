19211eaf5d6113b9e9a20cb386d8924ceba1fe8f
sergiocostamoreno <47816978+sergiocostamoreno@users.noreply.github.com>
2023-10-11 20:55:34 +0200

    Modules refactor

--- src/main/kotlin/infrastructure/config/ApplicationModule.kt
+++ src/main/kotlin/infrastructure/config/ApplicationModule.kt
@@ -1,11 +1,33 @@
 package com.example.infrastructure.config
 
 import com.example.infrastructure.adapter.output.DatabaseFactory
+import com.example.infrastructure.adapter.output.InMemoryScooters
+import com.example.infrastructure.adapter.output.InMemoryUsers
+import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.Application
+import io.ktor.server.application.install
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.server.resources.Resources
+import kotlinx.serialization.json.Json
 
 fun Application.rootModule() {
-    routesModule()
-    scooterModule()
+    contentNegotiation()
+    install(Resources)
+
+    val inMemoryScooters = InMemoryScooters()
+    val inMemoryUsers = InMemoryUsers()
+
+    routingModule(inMemoryScooters, inMemoryUsers)
     DatabaseFactory.init()
-    // users(GetUsersUseCase(inMemoryUsers))
+}
+
+fun Application.contentNegotiation() {
+    install(ContentNegotiation) {
+        json(
+            Json {
+                prettyPrint = true
+                isLenient = true
+            },
+        )
+    }
 }
