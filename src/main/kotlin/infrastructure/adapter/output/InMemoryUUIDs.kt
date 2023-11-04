3ed0bfc9203f9c3d78bce364d77f166e5f318b1b
sergiocostamoreno <47816978+sergiocostamoreno@users.noreply.github.com>
2023-11-04 11:22:39 +0100

    InMemoryUUIDs removed

--- src/main/kotlin/infrastructure/adapter/output/InMemoryUUIDs.kt
+++
@@ -1,9 +0,0 @@
-package com.example.infrastructure.adapter.output
-
-const val SCOOTER_ID_1 = 1
-const val SCOOTER_ID_2 = 2
-const val SCOOTER_ID_3 = 3
-
-const val USER_ID_1 = "UserA"
-const val USER_ID_2 = "UserB"
-const val USER_ID_3 = "UserC"
