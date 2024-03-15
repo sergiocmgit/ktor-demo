62a75a6802546a6f461b50364a143285deb36bd3
sergiocostamoreno <47816978+sergiocostamoreno@users.noreply.github.com>
2024-03-15 22:28:29 +0100

    RunScooterTest fixed

--- src/test/kotlin/component/RunScooterTest.kt
+++ src/test/kotlin/component/RunScooterTest.kt
@@ -5,12 +5,12 @@ import fixtures.DatabaseUtils.Companion.save
 import fixtures.builders.buildScooter
 import fixtures.builders.buildUser
 import fixtures.builders.randomUserId
-import io.ktor.client.request.get
+import io.ktor.client.request.post
 import io.ktor.http.HttpStatusCode.Companion.OK
 import io.ktor.server.testing.testApplication
+import kotlin.random.Random
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import kotlin.random.Random
 
 class RunScooterTest : ComponentTest() {
     @Test
@@ -21,7 +21,7 @@ class RunScooterTest : ComponentTest() {
             val userId = randomUserId().also { save(buildUser(userId = it)) }
             val scooterId = Random.nextInt().also { save(buildScooter(scooterId = it, status = LOCKED)) }
             // When
-            val response = client.get("/scooters/$scooterId/run/$userId")
+            val response = client.post("/scooters/$scooterId/run/$userId")
             // Then
             assertThat(response.status).isEqualTo(OK)
         }
